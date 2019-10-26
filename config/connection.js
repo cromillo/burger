277f5965b6edf01d36329c9257cc986a97cfac21
Claire Romillo <c_romillo@yahoo.com>
2019-10-26 15:37:07 -0700

    initiated Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -8,7 +12,7 @@ var connection = mysql.createConnection({
   password: "DigiKaia3!",
   database: "burgers_db"
 });
-
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
